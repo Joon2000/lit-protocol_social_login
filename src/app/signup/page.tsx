a04990f45cbdc2fed6ad57019ebc6e52fa5b2054
Joon <najoonwon22@gmail.com>
2023-09-22 19:23:27 +0900

    Add Signup

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,14 @@
+import { ORIGIN } from "@/src/utils/lit";
+import React from "react";
+
+const signup = () => {
+  const redirectUri = ORIGIN;
+
+  return (
+    <div>
+      <header className="text-center">Sign Up</header>
+    </div>
+  );
+};
+
+export default signup;
