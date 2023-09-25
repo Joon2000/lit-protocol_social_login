2ba2d737be9a6031f87cae31f5cc75eea2ec63ae
Joon <najoonwon22@gmail.com>
2023-09-25 15:15:58 +0900

    Add LoginMethods component

---
+++ src/components/LoginMethods.tsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import AuthMethods from "./AuthMethods";
+
+interface LoginProps {
+  handleGoogleLogin: () => Promise<void>;
+  signUp: any;
+  error?: Error;
+}
+
+export default function LoginMethods({
+  handleGoogleLogin,
+  signUp,
+  error,
+}: LoginProps) {
+  return (
+    <div className="container">
+      <div className="wrapper">
+        {error && (
+          <div className="alert alert--error">
+            <p>{error.message}</p>
+          </div>
+        )}
+        <>
+          <h1>Welcome back</h1>
+          <p>Access your lit wallet.</p>
+          <AuthMethods handleGoogleLogin={handleGoogleLogin} />
+          <div className="buttons-container">
+            <button type="button" className="btn btn--link" onClick={signUp}>
+              Need an account? Sign up
+            </button>
+          </div>
+        </>
+      </div>
+    </div>
+  );
+}
