746fd06f527dac737cebed3255ab8dc173a46f95
Joon <najoonwon22@gmail.com>
2023-09-25 15:14:04 +0900

    Add SignUp

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,14 +1,88 @@
-import { ORIGIN } from "../../utils/lit";
-import React from "react";
+import useAuthenticate from "@/hooks/useAuthenticate";
+import useAccounts from "@/hooks/useAccounts";
+import { ORIGIN, signInWithGoogle } from "../../utils/lit";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import useSession from "@/hooks/useSession";
+import Loading from "@/components/Loading";
+import Dashboard from "@/components/Dashboard";
+import SingUpMethods from "@/components/SignUpMethods";
 
-const signup = () => {
+const SignUp = () => {
   const redirectUri = ORIGIN;
 
-  return (
-    <div>
-      <header className="text-center">Sign Up</header>
-    </div>
-  );
+  const {
+    authMethod,
+    loading: authLoading,
+    error: authError,
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+  } = useAuthenticate(redirectUri);
+
+  const {
+    createAccount,
+    setCurrentAccount,
+    currentAccount,
+    loading: accountsLoading,
+    error: accountsError,
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+  } = useAccounts();
+
+  const {
+    initSession,
+    sessionSigs,
+    loading: sessionLoading,
+    error: sessionError,
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+  } = useSession();
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const router = useRouter();
+
+  const error = authError || accountsError || sessionError;
+
+  async function handleGoogleLogin() {
+    await signInWithGoogle(redirectUri);
+    // SignInWithGoogle을 하면 어떤 일이 일어나는거지??
+  }
+
+  useEffect(() => {
+    // If user is authenticated, create an account
+    if (authMethod) {
+      router.replace(window.location.pathname, undefined, { shallow: true });
+      createAccount(authMethod);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [authMethod, createAccount]);
+
+  useEffect(() => {
+    // If user is authenticated and has at least one account, initialize session
+    if (authMethod && currentAccount) {
+      initSession(authMethod, currentAccount);
+    }
+  }, [authMethod, currentAccount, initSession]);
+
+  if (authLoading) {
+    return (
+      <Loading copy={"Authenticating your credentials..."} error={error} />
+    );
+  }
+
+  if (accountsLoading) {
+    return <Loading copy={"Creating your account..."} error={error} />;
+  }
+
+  if (currentAccount && sessionSigs) {
+    return (
+      <Dashboard currentAccount={currentAccount} sessionSigs={sessionSigs} />
+    );
+  } else {
+    return (
+      <SingUpMethods
+        handleGoogleLogin={handleGoogleLogin}
+        goToLogin={() => router.push("/login")}
+        error={error}
+      />
+    );
+  }
 };
 
-export default signup;
+export default SignUp;
