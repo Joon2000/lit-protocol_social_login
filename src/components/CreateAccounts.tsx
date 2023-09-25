96d8bd47c4dcf3b8ed56ce865f8ba0c9ec4b06be
Joon <najoonwon22@gmail.com>
2023-09-25 15:15:20 +0900

    Add CreateAccounts component

---
+++ src/components/CreateAccounts.tsx
@@ -0,0 +1,28 @@
+interface CreateAccountsProp {
+  signUp: any;
+  error?: Error;
+}
+
+export default function CreateAccounts({ signUp, error }: CreateAccountsProp) {
+  return (
+    <div className="container">
+      <div className="wrapper">
+        {error && (
+          <div className="alert alert--error">
+            <p>{error.message}</p>
+          </div>
+        )}
+        <h1>Need a PKP?</h1>
+        <p>
+          There doesn&apos;t seem to be a Lit wallet associated with your
+          credentials. Create one today.
+        </p>
+        <div className="buttons-container">
+          <button onClick={signUp} className="btn btn--primary">
+            Sign up
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
